bf8d4d40f49718e843c97f1c0beb6f44c1cdd20d
Floc95 <florianconstant@gmail.com>
2014-05-06 10:01:52 +0200

    Ajout du repository et de la db

---
+++ server/repository.js
@@ -0,0 +1,24 @@
+function Repository()
+{
+	var self = this;
+
+	self.getUser = function(id) {
+		// TODO
+	};
+
+	self.getUsers = function(ids) {
+		// TODO
+	};
+
+	self.createUser = function(username, id) {
+		// TODO
+	};
+
+	self.getMessages = function(senderId, receiverIds, messagesCount) {
+		// TODO
+	};
+
+	self.createMessage = function(senderId, receiverIds, text) {
+		// TODO
+	}
+}
